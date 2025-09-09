c19b6c5da631179e8bcbc6f9ff5ae2f0250f296f
Sn0w123 <lucasskold49@gmail.com>
2025-09-09 19:59:28 +0200

    Update character background

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -16,6 +16,7 @@ import { useNavigation } from "@/hooks/use-navigation";
 import { useKeyboardNavigation } from "@/hooks/use-keyboard-navigation";
 import { handleHover } from "@/lib/util";
 
+import Background from "../../../public/bg/characters-background.webp";
 import Ianara from "../../../public/characters/lanara.webp";
 import Adeline from "../../../public/characters/adeline.webp";
 import Reiko from "../../../public/characters/reiko.webp";
@@ -129,102 +130,106 @@ export default function CharactersPage() {
     }, [setIsOpen]);
 
     return (
-        <div>
-            <NoiseFade
-                in={isOpen}
-                scale={scale}
-                duration={duration}
-                className="fixed inset-0 h-screen"
-            >
-                <div className="fixed text-6xl font-black top-17.5 left-39 z-10">
-                    <AnimatedBackground
-                        ref={charRefs.current[0]}
-                        hovered={selected === 0}
-                        onHover={(ref) =>
-                            handleHover(0, ref, setDroplets, setSelected)
-                        }
-                    >
-                        <Text>
-                            <TransitionLink href="/">BACK</TransitionLink>
-                        </Text>
-                    </AnimatedBackground>
+        <NoiseFade
+            in={isOpen}
+            scale={scale}
+            duration={duration}
+            className="fixed inset-0 h-screen"
+        >
+            <Image
+                src={Background}
+                alt="Background"
+                className="absolute inset-0 h-full w-full object-cover"
+                quality={100}
+            />
+            <div className="fixed text-6xl font-black top-17.5 left-39 z-10">
+                <AnimatedBackground
+                    ref={charRefs.current[0]}
+                    hovered={selected === 0}
+                    onHover={(ref) =>
+                        handleHover(0, ref, setDroplets, setSelected)
+                    }
+                >
+                    <Text>
+                        <TransitionLink href="/">BACK</TransitionLink>
+                    </Text>
+                </AnimatedBackground>
+            </div>
+            <div className="relative left-20 top-20 mt-0 flex flex-col">
+                <div className="flex items-baseline pointer-events-none">
+                    <Text className="text-9xl font-black">C</Text>
+                    <Text className="text-8xl font-black">HARACTERS</Text>
                 </div>
-                <div className="relative left-20 top-20 mt-0 flex flex-col">
-                    <div className="flex items-baseline pointer-events-none">
-                        <Text className="text-9xl font-black">C</Text>
-                        <Text className="text-8xl font-black">HARACTERS</Text>
-                    </div>
-                    <div className="relative left-10 flex flex-col">
-                        {characters.map((char, index) => (
-                            <CharacterText
-                                key={char.href}
-                                ref={charRefs.current[index + 1]}
-                                name={char.name}
-                                href={char.href}
-                                hovered={selected === index + 1}
-                                index={index}
-                                onHover={(ref) =>
-                                    handleHover(
-                                        index + 1,
-                                        ref,
-                                        setDroplets,
-                                        setSelected
-                                    )
-                                }
-                            />
-                        ))}
-                    </div>
-                    {droplets.map((d) => (
-                        <Droplet
-                            key={d.id}
-                            x={d.x}
-                            y={d.y}
-                            onComplete={() =>
-                                setDroplets((prev) =>
-                                    prev.filter((dd) => dd.id !== d.id)
+                <div className="relative left-10 flex flex-col">
+                    {characters.map((char, index) => (
+                        <CharacterText
+                            key={char.href}
+                            ref={charRefs.current[index + 1]}
+                            name={char.name}
+                            href={char.href}
+                            hovered={selected === index + 1}
+                            index={index}
+                            onHover={(ref) =>
+                                handleHover(
+                                    index + 1,
+                                    ref,
+                                    setDroplets,
+                                    setSelected
                                 )
                             }
                         />
                     ))}
                 </div>
+                {droplets.map((d) => (
+                    <Droplet
+                        key={d.id}
+                        x={d.x}
+                        y={d.y}
+                        onComplete={() =>
+                            setDroplets((prev) =>
+                                prev.filter((dd) => dd.id !== d.id)
+                            )
+                        }
+                    />
+                ))}
+            </div>
 
-                <div className="fixed right-0 -bottom-8 h-auto w-1/2 z-10">
-                    {characters.map((char, index) => (
-                        <CharacterHead
-                            key={char.href}
-                            image={char.image}
-                            alt={char.name}
-                            visible={selected === index + 1}
-                        />
-                    ))}
-                </div>
+            <div className="fixed right-0 -bottom-8 h-auto w-[100vw] md:w-1/2 z-10">
+                {characters.map((char, index) => (
+                    <CharacterHead
+                        key={char.href}
+                        image={char.image}
+                        alt={char.name}
+                        visible={selected === index + 1}
+                    />
+                ))}
+            </div>
 
-                <div className="fixed left-20 top-52">
-                    <Divider
-                        className="relative h-3 left-40 -top-5"
-                        scale={{ x: 2, y: 1 }}
-                    />
-                    <Divider
-                        className="relative h-5 w-70 -left-29 top-0"
-                        orientation="vertical"
-                        scale={{ x: 4, y: 1 }}
-                    />
-                    <Circle
-                        className="relative size-200 left-45"
-                        spinDuration={45}
-                    />
-                </div>
-                <div className="fixed right-20 bottom-10 w-60">
-                    <Divider
-                        className="relative h-2 right-60"
-                        scale={{ x: 6, y: 1 }}
-                    />
-                    <Circle
-                        className="fixed size-250 -bottom-200 -right-170"
-                        spinDuration={90}
-                    />
-                </div>
-            </NoiseFade>
-        </div>
+            <div className="fixed left-20 top-52">
+                <Divider
+                    className="relative h-3 left-40 -top-5"
+                    scale={{ x: 2, y: 1 }}
+                />
+                <Divider
+                    className="relative h-5 w-70 -left-29 top-0"
+                    orientation="vertical"
+                    scale={{ x: 4, y: 1 }}
+                />
+                <Circle
+                    className="relative size-200 left-45"
+                    spinDuration={45}
+                />
+            </div>
+            <div className="fixed right-20 bottom-10 w-60">
+                <Divider
+                    className="relative h-2 right-60"
+                    scale={{ x: 6, y: 1 }}
+                />
+                <Circle
+                    className="fixed size-250 -bottom-200 -right-170"
+                    spinDuration={90}
+                />
+            </div>
+        </NoiseFade>
     );
 }
