9ad4ecb4ed08ae57d4441cc537e42518e7cc4390
Sn0w123 <lucasskold49@gmail.com>
2025-09-10 14:59:12 +0200

    Set explicit image qualities

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    images: {
+        qualities: [25, 50, 75, 100],
+    },
     experimental: {
         reactCompiler: true,
     },
